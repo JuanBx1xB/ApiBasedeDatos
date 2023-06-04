2bcc67ce66ce1a5a163c12c68ee6a6c9285394a2
JuanBx1xB <juanbonilla0102@gmail.com>
2023-06-04 14:40:52 -0500

    ApiJuanB

---
+++ api/models/Nicolasapi.js
@@ -0,0 +1,17 @@
+/**
+ * Nicolasapi.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+  description: {type: 'string', },
+  stock: {type: 'string', },
+
+  },
+
+};
